2f9d5ac1b6859c5bf65eda05aad5af8116f1b666
tcvdh <tcvdh@outlook.com>
2025-02-08 01:03:58 +0100

    env fix

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -8,7 +8,7 @@ export interface Session {
 }
 
 export const authClient = createAuthClient({
-  baseURL: "https://" + process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   tokenStorage: "localStorage",
 });
 
